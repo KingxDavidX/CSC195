083133c726dd95e89fe112fdf151ce472579b5c2
KingxDavidX <davianh2016@gmail.com>
2025-04-01 16:04:53 -0600

    c++ intro

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -4,5 +4,13 @@ using namespace std;
 int main() {
 	cout << "Hello, World!" << endl;
 	cout << "Git is now tracking this project!" << endl;
+
+	int age;
+	//This is how you are supposed to do string without using "namespace std" which is above
+	std::cout << " Enter age: " << std::endl;
+	std::cin >> age;
+
+	cout << age << endl;
+
 	return 0;
 }

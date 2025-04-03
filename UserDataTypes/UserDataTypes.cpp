4fe0fc908427f51a59681e2a9aa7fb3b0c6d878f
KingxDavidX <davianh2016@gmail.com>
2025-04-03 15:40:12 -0600

    More follow along things done in class

--- UserDataTypes/UserDataTypes.cpp
+++ UserDataTypes/UserDataTypes.cpp
@@ -3,7 +3,16 @@
 
 using namespace std;
 
+//typedef double salary;    -- traditional c++ way
+
+//This is better than just making multiple declarations for different wages (in this example) because it is easier to change from float to double if you need a atribute to be more accurate or hold more data
+using salary = double;          //modern c++ way
+
 int main() {
+    salary wageReception = 14.4;
+    salary wageAnalyst = 18.5;
+    salary wageengineer = 32.4;
+
     cout << "User Data Types\n";
 
     Sandwich pastrami;

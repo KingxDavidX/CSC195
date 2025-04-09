bb31b7c93b3977e27bf45c1594e9d0af6ff4432a
KingxDavidX <davianh2016@gmail.com>
2025-04-09 16:26:33 -0600

    fixed typo

--- Containers/Containers.cpp
+++ Containers/Containers.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	string fruits[5] = { "Banana", "Kiwi", "Mango", "chikoo", "Strawbarry" };
+	string fruits[5] = { "Banana", "Kiwi", "Mango", "chikoo", "Strawberry" };
 
 	vector<int> numbers;
 	numbers.push_back(1);

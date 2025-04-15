c29995f788834d7edfb038c097e2a8dcabcd4313
KingxDavidX <davianh2016@gmail.com>
2025-04-14 18:44:03 -0600

    Assignment done

--- Containers/Containers.cpp
+++ Containers/Containers.cpp
@@ -5,6 +5,63 @@
 
 using namespace std;
 
+void arrayFunction() {
+	string daysOfWeek[7] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+	//the following code is required to get the size of the array, because sizeof doesn't do what you think it would
+	int arrSize = sizeof(daysOfWeek);
+	int elementSize = sizeof(daysOfWeek[0]);
+	int numElements = arrSize / elementSize;
+	cout << numElements << endl;
+
+	for (string day : daysOfWeek) {
+		cout << day << endl;
+	}
+
+	cout << "----------------------------------------------------------------------\n";
+}
+
+void vectorFunction() {
+	vector<int> numbers = { 1, 2, 3, 4, 5 };
+	//adding "6" and "7"
+	numbers.push_back(6);
+	numbers.push_back(7);
+	//removing "6" and "7" 
+	numbers.pop_back();
+	numbers.pop_back();
+
+	//Iterating through the vector and printing the numbers
+	for (int number : numbers) {
+		cout << number << endl;
+	}
+
+	cout << "----------------------------------------------------------------------\n";
+}
+
+void listFunction() {
+	list<string> fruit = { "Apple", "Banana", "Orange" };
+	fruit.emplace_front("Lemon");  // Emplace instead of push recommended by Intellisense
+	fruit.emplace_back("Kiwi");
+	fruit.remove("Apple");         // Removing apple
+
+	//list<string>::iterator creates an iterator and just specifies that the iterator is for a list that holds a string
+	for (list<string>::iterator it = fruit.begin(); it != fruit.end(); ++it) {
+		cout << *it << endl;
+	}
+
+	cout << "----------------------------------------------------------------------\n";
+}
+
+void mapFunction() {
+	map<string, int> groceryProducts = { {"Apple", 10}, {"Banana", 5}, {"Orange", 7} };
+	//changing how many Apples there are
+	groceryProducts["Apple"] = 20;
+
+	//A for each loop
+	for (const pair<string, int>& item : groceryProducts) {
+		cout << item.first << ": " << item.second << endl;
+	}
+}
+
 int main()
 {
 	//string fruits[5] = { "Banana", "Kiwi", "Mango", "chikoo", "Strawberry" };
@@ -93,18 +150,12 @@ int main()
 
 	//cout << pet.empty() << endl;
 
-	string daysOfWeek[7] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-	int arrSize = sizeof(daysOfWeek);
-	int elementSize = sizeof(daysOfWeek[0]);
-	int numElements = arrSize / elementSize;
-	cout << numElements << endl;
-
-	for (string day : daysOfWeek)
-	{
-		cout << day << endl;
-	}
-
-
-
+	arrayFunction();
+	vectorFunction();
+	listFunction();
+	mapFunction();
+	
 }
+
+
 
